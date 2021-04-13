140b123b0da39a3e2b501626802d7007e17c5ebb
navya-shine <69377970+navya-shine@users.noreply.github.com>
2021-04-13 09:43:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function take_snapshot ()
 }
 console.log('ml5 version:',ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/SJon9Mt7w//model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/SJon9Mt7w/model.json',modelLoaded);
 
 function modelLoaded() {
     console.log('Model Loaded!');
